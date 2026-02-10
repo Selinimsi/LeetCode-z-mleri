0033fa35112a481c1bcf35ceb81b1f7162061966
selin yıldırım <sy248359@gmail.com>
2026-02-10 22:48:55 +0300

    26. LeetCode problemi çözüldü

---
+++ LeteCodeCozumleri/RemoveDuplicatesfromSortedArray/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] nums = { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 };
+
+        int k = RemoveDuplicates(nums);
+
+        Console.WriteLine("Yeni uzunluk: " + k);
+
+        Console.Write("Benzersiz elemanlar: ");
+        for (int i = 0; i < k; i++)
+        {
+            Console.Write(nums[i] + " ");
+        }
+    }
+
+    static int RemoveDuplicates(int[] nums)
+    {
+        if (nums.Length == 0)
+            return 0;
+
+        int k = 1;
+
+        for (int i = 1; i < nums.Length; i++)
+        {
+            if (nums[i] != nums[i - 1])
+            {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+
+        return k;
+    }
+}
---
+++ LeteCodeCozumleri/RemoveDuplicatesfromSortedArray/Readme.md
@@ -0,0 +1,7 @@
+Remove Duplicates from Sorted Array
+https://leetcode.com/problems/remove-duplicates-from-sorted-array/description/
+Artmayan sıradanums sıralanmış bir tamsayı dizisi verildiğinde , her benzersiz elemanın yalnızca bir kez görünmesi sağlanacak şekilde yinelenen elemanları yerinde kaldırın . Elemanların göreceli sıralaması aynı kalmalıdır .
+
+Dizideki  benzersiz eleman sayısını olarak kabul edin nums. Yinelenenleri çıkardıktan sonra, benzersiz eleman sayısını döndürün  .k​​​​​​​k
+
+Dizinin ilk  k elemanları,  sıralı düzendenums  benzersiz sayılar içermelidir . Dizin numarasından sonraki elemanlar   göz ardı edilebilir
