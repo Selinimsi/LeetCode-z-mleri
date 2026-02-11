82af67f6db3d7b74462bdcfda8da3a56b459fc37
selin yıldırım <sy248359@gmail.com>
2026-02-11 20:24:34 +0300

    27. Remove Element Leetcode problemi çöz

---
+++ LeteCodeCozumleri/RemoveElement/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] nums = { 3, 2, 2, 3 };
+        int val = 3;
+
+        int newLength = RemoveElement(nums, val);
+
+        Console.WriteLine("Yeni uzunluk: " + newLength);
+        Console.Write("Yeni dizi: ");
+
+        for (int i = 0; i < newLength; i++)
+        {
+            Console.Write(nums[i] + " ");
+        }
+    }
+
+    public static int RemoveElement(int[] nums, int val)
+    {
+        int index = 0;
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] != val)
+            {
+                nums[index] = nums[i];
+                index++;
+            }
+        }
+
+        return index;
+    }
+}
---
+++ LeteCodeCozumleri/RemoveElement/readme.md
@@ -0,0 +1,3 @@
+Remove Element
+https://leetcode.com/problems/remove-element/description/
+Verilen bir tamsayı dizisi numsve bir tamsayı için, dizideki valtüm tamsayıları yerinde kaldırın . Elemanların sırası değiştirilebilir. Ardından, tamsayıya eşit olmayan elemanların sayısını döndürün .
