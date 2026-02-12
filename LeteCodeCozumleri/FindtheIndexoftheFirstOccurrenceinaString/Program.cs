2a18c0071c5aad4cdc348b335850a688fb89c74e
selin yıldırım <sy248359@gmail.com>
2026-02-12 15:34:37 +0300

    LeetCode 28.soru çözüldü

---
+++ LeteCodeCozumleri/FindtheIndexoftheFirstOccurrenceinaString/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Solution solution = new Solution();
+
+        Console.Write("Haystack giriniz: ");
+        string haystack = Console.ReadLine();
+
+        Console.Write("Needle giriniz: ");
+        string needle = Console.ReadLine();
+
+        int result = solution.StrStr(haystack, needle);
+
+        Console.WriteLine("Sonuç: " + result);
+    }
+}
+
+public class Solution
+{
+    public int StrStr(string haystack, string needle)
+    {
+        if (needle == "") return 0;
+
+        for (int i = 0; i <= haystack.Length - needle.Length; i++)
+        {
+            if (haystack.Substring(i, needle.Length) == needle)
+            {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
---
+++ LeteCodeCozumleri/FindtheIndexoftheFirstOccurrenceinaString/readme.md
@@ -0,0 +1,8 @@
+Bir Dizideki İlk Geçişin İndeksini Bulun
+Çözüldü
+Kolay
+Konular
+premium kilit simgesi
+Şirketler
+Verilen iki dize olan `<string> needle` ve ` <string>` için, `<string>`'nin `<string>`'de ilk geçtiği yerin haystackindeksini döndürün ; eğer `<string>`, `<string>` 'nin bir parçası değilse `null` döndürün
+https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/description/
