2cb1fbd8054189369cde7f44a5026ac4472d933a
selin yıldırım <sy248359@gmail.com>
2026-02-17 23:28:23 +0300

    58. soru cozuldu

---
+++ LeteCodeCozumleri/LengthofLastWord/Program.cs
@@ -0,0 +1,21 @@
+﻿public class Solution
+{
+    public int LengthOfLastWord(string s)
+    {
+        int length = 0;
+        int i = s.Length - 1;
+
+        // sondaki boşlukları geç
+        while (i >= 0 && s[i] == ' ')
+            i--;
+
+        // son kelimenin uzunluğunu say
+        while (i >= 0 && s[i] != ' ')
+        {
+            length++;
+            i--;
+        }
+
+        return length;
+    }
+}
