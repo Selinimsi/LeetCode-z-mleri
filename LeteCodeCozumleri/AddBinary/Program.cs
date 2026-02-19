45222d9530cf1ca22edd02e920b322d22484dd10
selin yıldırım <sy248359@gmail.com>
2026-02-19 21:44:15 +0300

    67.soru çözüldü

---
+++ LeteCodeCozumleri/AddBinary/Program.cs
@@ -0,0 +1,33 @@
+﻿public class Solution
+{
+    public string AddBinary(string a, string b)
+    {
+        int i = a.Length - 1;
+        int j = b.Length - 1;
+        int carry = 0;
+
+        System.Text.StringBuilder sb = new System.Text.StringBuilder();
+
+        while (i >= 0 || j >= 0 || carry > 0)
+        {
+            int sum = carry;
+
+            if (i >= 0)
+            {
+                sum += a[i] - '0';
+                i--;
+            }
+
+            if (j >= 0)
+            {
+                sum += b[j] - '0';
+                j--;
+            }
+
+            sb.Insert(0, sum % 2);
+            carry = sum / 2;
+        }
+
+        return sb.ToString();
+    }
+}
